f3a87235d78e0152db055a45434e5ab548f8effb
BigBeardYT <627944524@qq.com>
2023-10-10 11:17:21 +0800

    "add"

---
+++ Ginzburg-Landau/attack.py
@@ -0,0 +1,109 @@
+import sys
+
+import torch
+
+from my_utils.fgsm import generate_fgsm_noise
+from my_utils.load_models import get_model
+from my_utils.pgd import generate_pgd_noise, generate_bim_noise
+import torch.nn as nn
+sys.path.append("..")
+from my_utils.load_datasets import get_datasets
+from LeNet5 import LeNet5
+# 导包，自定义的攻击函数
+from my_utils.adversarial_attack import noise_attack
+
+# 模型（三通道的图，应该加载不同的模型）
+Ising_LeNet_model_name = 'IsingLeNet'
+LeNet_model_name = 'LeNet'
+""" ######## 以下参数训练之前手动设置 ######### """
+attacked_batch_size = 32
+attacked_num_epochs = 10
+lr = 0.01
+data_name = 'MNIST'
+model_name = LeNet_model_name
+num_classes = 10
+""" ######## 以上参数训练之前手动设置 ######### """
+
+
+# 攻击时使用的数据集大小
+attack_used_batch_size = 32
+
+# 加载数据集
+train_dataset, test_dataset, \
+    train_loader, test_loader = \
+    get_datasets(batch_size=attack_used_batch_size, data_name=data_name)
+epsilons = [0.01, 0.05, 0.1, 0.2, 0.25, 0.3]
+in_features = 1
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+batch_size = 32
+num_epochs = 5
+# 攻击方法
+noise_name = 'PGD'
+for i in range(1, 2):
+    # 模型对抗攻击
+    print('第{}次攻击, 模型 {}, 数据集 {}'.format(i, model_name, data_name))
+    # 加载模型
+    # attacked_model = get_model(model_name, in_features=in_features, num_classes=num_classes).to(device)
+    attacked_model = LeNet5().to(device)
+    # 普通模型直接存储的路径
+    attacked_model_params_path = '../savemodel/' + data_name + '_' + 'GinzburgLandau' \
+                                 + '_bz' + str(batch_size) + '_ep' + str(num_epochs) + \
+                                 '_lr' + str(lr) + '_seedNone' + str(i) + '.pth'
+
+    # 对抗样本训练后的路径
+    # attacked_model_params_path = '../trained_model/' + data_name + '_' + model_name + '_' + 'PGD' \
+    #                              + '_train' + '_bz' + str(batch_size) + '_ep' + str(num_epochs) + \
+    #                              '_lr' + str(lr) + '_seedNone' + str(i) + '.pth'
+
+    # attacked_model_params_path = '../trades_trained_model/CiFar10_ResNet_Trades_train_bz128_ep100_lr0
+    # .01_seedNone1.pth'
+
+    print('模型参数所在位置\n' + attacked_model_params_path)
+
+    # 加载参数
+    attacked_model.load_state_dict(torch.load(attacked_model_params_path))
+
+    """ 扰动攻击 """
+    for epsilon in epsilons:
+
+        criterion = nn.CrossEntropyLoss()
+        total_correct = 0
+        total_samples = 0
+        for images, labels in test_loader:
+
+            images, labels = images.to(device), labels.to(device)
+            # 原始模型
+            init_outputs = attacked_model(images)
+            # 预测结果
+            _, pred = torch.max(init_outputs, 1)
+
+            total_samples += init_outputs.shape[0]
+            # 生成PGD噪声 默认20步长攻击
+            if noise_name == 'PGD':
+                iters = generate_pgd_noise(attacked_model, images, labels, criterion, device,
+                                           epsilon=epsilon, num_iter=20, minv=0, maxv=1)
+            # BIM噪声
+            elif noise_name == 'BIM':
+                iters = generate_bim_noise(attacked_model, images, labels, criterion, device,
+                                           epsilon=epsilon, iters=5, minv=0, maxv=1)
+            # FGSM攻击噪声
+            elif noise_name == 'FGSM':
+                iters = generate_fgsm_noise(attacked_model, images, labels, criterion, device,
+                                            epsilon=epsilon, minv=0, maxv=1)
+
+            eta, adv_images = iters
+
+            # 攻击后的图片的预测结果
+            final_outputs = attacked_model(adv_images)
+            _, final_preds = torch.max(final_outputs, 1)
+
+            final_preds_list = final_preds.tolist()
+            src_labels = labels.tolist()
+            for j in range(len(final_preds_list)):
+                if final_preds_list[j] == src_labels[j]:
+                    total_correct += 1
+
+        final_acc = total_correct / float(len(test_loader) * attack_used_batch_size)  # 计算整体准确率
+        print("Epsilon: {}\tTest Accuracy = {} / {} = {:.2f}".format(
+            epsilon, total_correct, len(test_loader) * attack_used_batch_size, final_acc * 100))
+
--- Ginzburg-Landau/test.py
+++ Ginzburg-Landau/test.py
@@ -8,36 +8,17 @@ import torch.nn.functional as F
 
 # 定义 LeNet-5 模型
 from my_utils.load_datasets import get_datasets
-
-
-class LeNet5(nn.Module):
-    def __init__(self):
-        super(LeNet5, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+from LeNet5 import LeNet5
 
 
 # 定义自定义的损失函数
 def custom_loss(outputs, targets, model, alpha=1.0, beta=1.0):
-    # 鲁棒性损失：这里我们只使用交叉熵损失作为示例，但在实际应用中，应使用对抗损失
+    # 鲁棒性损失：为简单起见，我们使用模型的 L2 范数，但更复杂的度量也可以使用，但在实际应用中，应使用对抗损失
+    # robustness_loss = torch.norm(torch.stack([torch.norm(p) for p in model.parameters()]))
     robustness_loss = nn.CrossEntropyLoss()(outputs, targets)
 
-    # 泛化性损失：为简单起见，我们使用模型的 L2 范数，但更复杂的度量也可以使用
-    generalization_loss = torch.norm(torch.stack([torch.norm(p) for p in model.parameters()]))
+    # 泛化性损失：使用交叉熵损失作为示例
+    generalization_loss = nn.CrossEntropyLoss()(outputs, targets)
 
     # 总损失
     total_loss = alpha * robustness_loss + beta * generalization_loss
@@ -45,29 +26,33 @@ def custom_loss(outputs, targets, model, alpha=1.0, beta=1.0):
 
 
 batch_size = 32
+num_epochs = 5
+learning_rate = 0.01
 data_name = 'MNIST'
 # 加载数据集
 train_datasets, test_datasets, train_loader, test_loader = get_datasets(batch_size=batch_size, data_name=data_name)
 # 创建模型，优化器和设备
 model = LeNet5()
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 
 # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model.to(device)
 
 # 训练
-num_epochs = 5
+
+best_acc = 0.0
 for epoch in range(num_epochs):
+    model.train()
     for i, (images, labels) in enumerate(train_loader):
         images = images.to(device)
         labels = labels.to(device)
 
         # 前向传播
         outputs = model(images)
-        # _, pred = torch.max(outputs, 1)
-
-
+        _, pred = torch.max(outputs, 1)
+        correct_pred = (pred == labels).sum()
+        acc = correct_pred / labels.shape[0]
         # 计算损失
         loss = custom_loss(outputs, labels, model)
 
@@ -77,4 +62,26 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         if (i + 1) % 100 == 0:
-            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}, '
+                  f'Accuracy: {acc.item():.4f}')
+
+    # 模型测试
+    model.eval()
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, pred = torch.max(outputs, 1)
+            correct_pred = torch.sum(pred == labels)
+            acc = correct_pred / labels.shape[0]
+
+        print('Epoch: [{}/{}], Test_Acc: {:.4f}'.format(epoch+1, num_epochs, acc.item()))
+        save_name = 'MNIST_GinzburgLandau'
+        if acc.item() > best_acc and epoch > num_epochs*0.5:
+            best_acc = acc.item()
+            # 存储模型
+            best_model_params_path = '../savemodel/' + save_name + '_bz' + str(batch_size) + '_ep' + str(num_epochs) + \
+                                     '_lr' + str(learning_rate) + 'seedNone2' + '.pth'
+            torch.save(model.state_dict(), best_model_params_path)
+
+
